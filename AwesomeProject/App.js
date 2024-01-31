1fa46f92e245b27fdd387ed583bf176ca3e6f7ff
darkcat974 <florian.dajon@epitech.eu>
2024-01-31 17:54:50 +0400

    done until 4

--- AwesomeProject/App.js
+++ AwesomeProject/App.js
@@ -1,10 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View, TextInput, TouchableOpacityBase } from 'react-native';
+export const ButtonPersonalized = ({name, action}) => {
+  return (
+    <TouchableOpacity onPress={action} style={styles.button_st}>
+      {name}
+      </TouchableOpacity>
+  );
+}
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text >Ecran1</Text>
+      <TextInput placeholder="Ecrir ici" style={styles.input_colors}/>
+      <TouchableOpacity onPress={() => {
+    console.log('bonjour');
+  }} style={styles.button_st}>Press moi</TouchableOpacity>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +28,18 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  input_colors: {
+    border: "2px solid #00ffbb",
+    borderRadius: "4px",
+    backgroundColor: "#00ffbb",
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  button_st : {
+    border: "2px solid #fff",
+    borderRadius: "4px",
+    backgroundColor: "#e89999",
+    alignItems: 'center',
+    justifyContent: 'center',
+  }
 });
